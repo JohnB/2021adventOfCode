2da3beedf4ff50735d7b5d3d7524c33e9a67a60e
John Baylor <john.baylor@gmail.com>
2021-11-29 00:10:31 -0800

    Use AOC code from livebook via recompilation

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,7 +1,11 @@
-defmodule Livebook.AdventOfCode do
+defmodule AdventOfCode do
   @moduledoc """
   Helper module for dealing with text input from the AOC puzzles.
   Originally created for the 2021 competition.
+
+  To use from LiveBook:
+    IEx.Helpers.c("lib/advent_of_code.ex")
+    alias AdventOfCode, as: AOC
   """
 
   def as_single_lines(multiline_text) do
@@ -19,12 +23,34 @@ defmodule Livebook.AdventOfCode do
   end
 
   def delimited_by_spaces(text) do
-    String.split(~r/\s+/)
+    text
+    |> String.split(~r/\s+/)
   end
 
   def delimited_by_colons(text) do
-    String.split(~r/\:/)
+    text
+    |> String.split(~r/\:/)
   end
 
+  # Being a multi-line input element, this gets all of it (within reason).
+  def get_kino_textarea(inputname) do
+    IO.getn(inputname, 1_000_000)
+  end
+
+  # Being a multi-line input element, this gets all of it (within reason).
+  def get_kino_integer(inputname) do
+    IO.gets(inputname)
+    |> String.trim()
+    |> String.to_integer()
+  end
+
+  def day_of_advent(), do: get_kino_integer("Day of Advent:")
+  def part1example(), do: get_kino_textarea("Part 1 Example: ")
+  def part1input(), do: get_kino_textarea("Part 1 Puzzle Input: ")
+  def p1data(), do: (IO.gets("Part 1 Input Data") =~ "example" && part1example()) || part1input()
+
+  def part2example(), do: get_kino_textarea("Part 2 Example: ")
+  def part2input(), do: get_kino_textarea("Part 2 Puzzle Input: ")
+  def p2data(), do: (IO.gets("Part 2 Input Data") =~ "example" && part2example()) || part2input()
 end
 
